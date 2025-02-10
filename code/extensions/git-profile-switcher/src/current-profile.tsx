66743f95ea0c611fed0d31e6054cc9b6307946fd
Y.Yamamoto <tuki0918@gmail.com>
2025-02-10 20:23:13 +0900

    ♻️ Refactor current profile component to use usePromise for data fetching

--- code/extensions/git-profile-switcher/src/current-profile.tsx
+++ code/extensions/git-profile-switcher/src/current-profile.tsx
@@ -1,45 +1,40 @@
 import { execPromise } from "@/lib";
 import type { Scope } from "@/types";
 import { Icon, List } from "@raycast/api";
-import { useEffect, useState } from "react";
+import { usePromise } from "@raycast/utils";
 
 type Profile = { scope: Scope; name?: string | null; email?: string | null };
 
 export default function Command() {
-  const [profiles, setProfiles] = useState<Profile[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    getProfiles().then((profiles) => {
-      setProfiles([...profiles, { scope: "local", name: "---", email: "---" }]);
-      setLoading(false);
-    });
-  }, []);
+  const { isLoading, data } = usePromise(async () => {
+    const profiles = await getProfiles();
+    return [...profiles, { scope: "local", name: "---", email: "---" }];
+  });
 
   return (
-    <List isLoading={loading} isShowingDetail searchBarPlaceholder="Select Git scope">
-      {profiles.map((profile) => (
-        <List.Item
-          accessories={[{ text: "scope" }]}
-          key={profile.scope}
-          title={profile.scope}
-          icon={Icon.Bird}
-          detail={
-            <List.Item.Detail
-              metadata={
-                <List.Item.Detail.Metadata>
-                  <List.Item.Detail.Metadata.Label title="user.name" text={profile.name || ""} />
-                  <List.Item.Detail.Metadata.Label title="user.email" text={profile.email || ""} />
-                  <List.Item.Detail.Metadata.TagList title="scope">
-                    <List.Item.Detail.Metadata.TagList.Item text={profile.scope} />
-                  </List.Item.Detail.Metadata.TagList>
-                </List.Item.Detail.Metadata>
-              }
-            />
-          }
-        />
-      ))}
+    <List isLoading={isLoading} isShowingDetail searchBarPlaceholder="Select Git scope">
+      {data &&
+        data.map((profile) => (
+          <List.Item
+            accessories={[{ text: "scope" }]}
+            key={profile.scope}
+            title={profile.scope}
+            icon={Icon.Bird}
+            detail={
+              <List.Item.Detail
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    <List.Item.Detail.Metadata.Label title="user.name" text={profile.name || ""} />
+                    <List.Item.Detail.Metadata.Label title="user.email" text={profile.email || ""} />
+                    <List.Item.Detail.Metadata.TagList title="scope">
+                      <List.Item.Detail.Metadata.TagList.Item text={profile.scope} />
+                    </List.Item.Detail.Metadata.TagList>
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+          />
+        ))}
     </List>
   );
 }
