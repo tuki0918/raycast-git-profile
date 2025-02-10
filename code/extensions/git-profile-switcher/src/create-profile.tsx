57cb3fb87ddb20a41186bdfd38ec7218b0c6bcee
Y.Yamamoto <tuki0918@gmail.com>
2025-02-10 22:14:17 +0900

    ✨ Introduce create profile component for user data submission

---
+++ code/extensions/git-profile-switcher/src/create-profile.tsx
@@ -0,0 +1,48 @@
+import { Action, ActionPanel, Form, showToast, Toast, LocalStorage } from "@raycast/api";
+import { useForm, FormValidation } from "@raycast/utils";
+import { setData } from "@/utils";
+
+interface FormValues {
+  name: string;
+  email: string;
+}
+
+export default function Command() {
+  const { handleSubmit, itemProps } = useForm<FormValues>({
+    async onSubmit(values) {
+      const data = await LocalStorage.getItem(values.email);
+      if (data !== undefined) {
+        showToast({
+          style: Toast.Style.Failure,
+          title: "Error!",
+          message: "The email is already in use.",
+        });
+        return;
+      }
+      await setData(values);
+      showToast({
+        style: Toast.Style.Success,
+        title: "Success!",
+        message: `${values.name} profile created.`,
+      });
+    },
+    validation: {
+      name: FormValidation.Required,
+      email: FormValidation.Required,
+    },
+  });
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.Description text="Profile" />
+      <Form.TextField title="user.name" placeholder="Your name" {...itemProps.name} />
+      <Form.TextField title="user.email" placeholder="test@example.com" {...itemProps.email} />
+    </Form>
+  );
+}
