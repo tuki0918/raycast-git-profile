5ad607f291a508ec27956f106b5815143bf000b3
Y.Yamamoto <tuki0918@gmail.com>
2025-02-10 22:14:05 +0900

    ✨ Introduce storage utility for profile data management

---
+++ code/extensions/git-profile-switcher/src/utils/storage.ts
@@ -0,0 +1,16 @@
+import { LocalStorage } from "@raycast/api";
+import type { Profile } from "@/types";
+
+type Values = {
+  [key: string]: string;
+};
+
+export async function setData(value: Profile) {
+  const data = JSON.stringify(value);
+  await LocalStorage.setItem(value.email, data);
+}
+
+export async function getData(): Promise<Profile[]> {
+  const data = await LocalStorage.allItems<Values>();
+  return Object.values(data).map((v) => JSON.parse(v) as Profile);
+}
