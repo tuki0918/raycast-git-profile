4d0d7293b0bb6c61c5c729b2252cb5b2286d1a49
Y.Yamamoto <tuki0918@gmail.com>
2025-02-11 00:07:32 +0900

    ✨ Introduce CreateProfileForm component for user profile creation

--- code/extensions/git-profile-switcher/src/create-profile.tsx
+++ code/extensions/git-profile-switcher/src/create-profile.tsx
@@ -1,48 +1,5 @@
-import { Action, ActionPanel, Form, showToast, Toast, LocalStorage } from "@raycast/api";
-import { useForm, FormValidation } from "@raycast/utils";
-import { setData } from "@/utils";
-
-interface FormValues {
-  name: string;
-  email: string;
-}
+import CreateProfileForm from "@/components/CreateProfileForm";
 
 export default function Command() {
-  const { handleSubmit, itemProps } = useForm<FormValues>({
-    async onSubmit(values) {
-      const data = await LocalStorage.getItem(values.email);
-      if (data !== undefined) {
-        await showToast({
-          style: Toast.Style.Failure,
-          title: "Error!",
-          message: "The email is already in use.",
-        });
-        return;
-      }
-      await setData(values);
-      await showToast({
-        style: Toast.Style.Success,
-        title: "Success!",
-        message: `${values.name} profile created.`,
-      });
-    },
-    validation: {
-      name: FormValidation.Required,
-      email: FormValidation.Required,
-    },
-  });
-
-  return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} />
-        </ActionPanel>
-      }
-    >
-      <Form.Description text="Profile" />
-      <Form.TextField title="user.name" placeholder="Your name" {...itemProps.name} />
-      <Form.TextField title="user.email" placeholder="test@example.com" {...itemProps.email} />
-    </Form>
-  );
+  return <CreateProfileForm />;
 }
